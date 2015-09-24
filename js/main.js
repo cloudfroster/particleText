77927e097e969d7e9b76fc819b0ae0eccbfb75cb
marchen <844033231@qq.com>
2015-09-24 18:13:48 +0800

    init commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+~ function() {
+  var requestAnimationFrame = window.requestAnimationFrame;
+  var updateButton = document.querySelector('#updateButton');
+  var updateText = document.querySelector('#updateText');
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+  var TEXT = 'HELLO WORLD';
+  var WIDTH, HEIGHT;
+  var mouseX = 1000,
+    mouseY = 1000;
+  window.onresize = main;
+  window.addEventListener("mousemove", mouseMove, false);
+  window.addEventListener("touchmove", touchMove, false);
+
+  updateButton.onclick = function(){
+    TEXT = updateText.value || TEXT;
+    main();
+  }
+
+  main();
+  function main() {
+
+    setWidthHeight(); // 设置宽高
+
+    var particles = [];
+    //ctx.globalAlpha = 1;
+    //ctx.globalCompositeOperation = "lighter"; 
+    // 新建粒子
+    createParticles();
+    // 动画循环
+    function run() {
+      requestAnimationFrame(run);
+      // 清除画布
+      ctx.clearRect(0, 0, WIDTH, HEIGHT);
+      ctx.beginPath();
+
+      particles.forEach(function(particle, index) {
+        particle.tick(mouseX, mouseY, ctx);
+      });
+    }
+    run();
+
+    // 新建粒子
+    function createParticles() {
+      if(WIDTH < 480) {
+        ctx.font = "30px '微软雅黑'";
+      }else {
+        ctx.font = "80px '微软雅黑'";
+      }
+      // 创建渐变
+      var gradient = ctx.createLinearGradient(0, 0, WIDTH, 0);
+      gradient.addColorStop("0", "white");
+      gradient.addColorStop("0.2", "gold");
+      gradient.addColorStop("0.5", "pink");
+      gradient.addColorStop("0.7", "white");
+      gradient.addColorStop("1.0", "red");
+      // 用渐变填色
+      ctx.strokeStyle = gradient;
+      var textWidth = ctx.measureText(TEXT).width;
+      ctx.strokeText(TEXT, WIDTH/2 - textWidth/2, HEIGHT/2);
+      // 从canvas中抽象出粒子
+      var imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+      for (var i = 0, len = imgData.data.length; i < len; i = i + 4) {
+        var r = imgData.data[i];
+        var g = imgData.data[i + 1];
+        var b = imgData.data[i + 2];
+        var a = imgData.data[i + 3];
+        var x = Math.floor((i % (WIDTH * 4)) / 4);
+        var y = Math.floor(i / (WIDTH * 4));
+        var color = 'rgba(' + r + ',' + g + ',' + b + ','+ a +')';
+        if (a) {
+          particles.push(new Particle({
+            x: x,
+            y: y,
+            color: color
+          }));
+        }
+      }
+    }
+  };
+
+
+
+  // 设置canvas为全屏
+  function setWidthHeight() {
+    WIDTH = canvas.width = parseInt(window.getComputedStyle(document.querySelector('body')).width);
+    HEIGHT = canvas.height = parseInt(window.getComputedStyle(document.querySelector('body')).height);
+  }
+  // 鼠标移动
+  function mouseMove(event) {
+    event = event || window.event;
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+  }
+  // 触摸屏
+  function touchMove(event) {
+    event = event || window.event;
+    var touch = event.touches[0];
+    mouseX = touch.pageX;
+    mouseY = touch.pageY;
+  }
+}();
